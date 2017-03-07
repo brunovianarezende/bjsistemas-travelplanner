5903f55fc785a8a8290dff0b5b85102ce15c588a
Bruno Rezende <brunovianarezende@gmail.com>
2017-03-07 13:53:53 -0300

    project structure created with giter8

---
+++ README.md
@@ -0,0 +1,12 @@
+# Travel Planner #
+
+## Build & Run ##
+
+```sh
+$ cd Travel_Planner
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/nom/bruno/travelplanner/TravelPlannerServlet.scala
@@ -0,0 +1,16 @@
+package nom.bruno.travelplanner
+
+import org.scalatra._
+
+class TravelPlannerServlet extends TravelPlannerStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
